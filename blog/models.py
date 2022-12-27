c1738f02dc26759e14f888d4cc3b9efe467be684
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-12-27 17:18:03 +0600

    db backup formated

--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,19 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=100, blank=True, default='')
 	body = models.TextField(blank=True, default='')
-	# ForeignKey -> many-to-one relationship between current model and auth.user
-	# One user can be the author of many posts, but one post will have only one author
-	# related_name to set a custom access name for the posts model instead of default post_set
-	# It is added to the User serializer to complete the many-to-one relationship
+	"""
+	ForeignKey -> many-to-one relationship between current model and auth.user
+	One user can be the author of many posts, but one post will have only one author
+	related_name to set a custom access name for the posts model instead of default post_set
+	It is added to the User serializer to complete the many-to-one relationship
+	"""
 	author = models.ForeignKey('auth.user', related_name='posts', on_delete=models.CASCADE)
 
 	class Meta:
 		ordering = ['created']
+
+	def __str__(self):
+		return self.title
 
 class Comment(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
@@ -23,6 +28,9 @@ class Comment(models.Model):
 	class Meta:
 		ordering = ['created']
 
+	def __str__(self):
+		return self.body
+
 class Category(models.Model):
 	name = models.CharField(max_length=60, blank=False, default='')
 	author = models.ForeignKey('auth.user', related_name='categories', on_delete=models.CASCADE)
@@ -30,3 +38,6 @@ class Category(models.Model):
 
 	class Meta:
 		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
--- blog/serializers.py
+++ blog/serializers.py
@@ -2,9 +2,17 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from blog.models import Post, Comment, Category
 
+class CommentSerializer(serializers.ModelSerializer):
+	# author = serializers.ReadOnlyField(source='author.username')
+
+	class Meta:
+		model = Comment
+		fields = ['id', 'body', 'author', 'post']
+
 class UserSerializer(serializers.ModelSerializer):
 	posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-	comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+	# comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+	comments = CommentSerializer(required=False, read_only=True)
 	categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
 	class Meta:
@@ -18,13 +26,6 @@ class PostSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Post
 		fields = ['id', 'title', 'body', 'author', 'comments', 'categories']
-
-class CommentSerializer(serializers.ModelSerializer):
-	author = serializers.ReadOnlyField(source='author.username')
-
-	class Meta:
-		model = Comment
-		fields = ['id', 'body', 'author', 'post']
 
 class CategorySerializer(serializers.ModelSerializer):
 	author = serializers.ReadOnlyField(source='author.username')
